0a87cf5036a7033453e42bd6a4a7ec232e0ecfa5
IlinArtem78 <artem.ilin1994@gmail.com>
2024-06-02 07:09:34 +0300

    Создание проекта. Задание 3.2.3, 4

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        string myName;
+        myName = "Artem";
+
+        Console.WriteLine(myName);
+
+        Console.ReadKey();
+    }
+}
