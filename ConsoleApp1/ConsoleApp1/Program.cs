137c43eebbfb2ac05c8c26e6b4ae50bf977adf87
IlinArtem78 <artem.ilin1994@gmail.com>
2024-06-17 09:45:29 +0300

    5.2.8

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,9 +6,10 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 class MainClass
 {
 
-    static string ShowColor()  //static - модификатор метода
+    static string ShowColor(string username, int userage)  //static - модификатор метода
     {
-        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+        Console.WriteLine("{0}, {1} лет \nНапишите свой любимый цвет на английском с маленькой буквы ", username, userage);
         var color = Console.ReadLine();
 
         switch (color)
@@ -18,6 +19,7 @@ class MainClass
                 Console.ForegroundColor = ConsoleColor.Black;
 
                 Console.WriteLine("Your color is red!");
+               
                 break;
 
             case "green":
@@ -38,7 +40,7 @@ class MainClass
 
                 Console.WriteLine("Your color is yellow!");
                 break;
-                
+
         }
         return color;
     }
@@ -49,69 +51,93 @@ class MainClass
 
     public static void Main(string[] args)
     {
-      /*  var (name, age) = ("Евгения", 27);
 
-        Console.WriteLine("Мое имя: {0}", name);
-        Console.WriteLine("Мой возраст: {0}", age);
+        (string F_Name, string S_Name, string login, int l_login, bool Pit, byte Age, string[] favcolor) User;
+        /*   var (name, age) = ("Евгения", 27);
 
-        Console.Write("Введите имя: ");
-        name = Console.ReadLine();
-        Console.Write("Введите возрас с цифрами:");
-        age = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Мое имя: {0}", name);
+           Console.WriteLine("Мой возраст: {0}", age);
 
-        Console.WriteLine("Ваше имя: {0}", name);
-        Console.WriteLine("Ваш возраст: {0}", age); */
+           Console.Write("Введите имя: ");
+           name = Console.ReadLine();
+           Console.Write("Введите возрас с цифрами:");
+           age = Convert.ToInt32(Console.ReadLine());
 
-     //   ShowColor();
+           Console.WriteLine("Ваше имя: {0}", name);
+           Console.WriteLine("Ваш возраст: {0}", age); 
 
-        GetDataFromConsole();
+         //   ShowColor();*/
 
-        int[] papa =  GetArray();
+           GetDataFromConsole();
+           int[] array = new int[5];
+
+           for (int i = 0; i < array.Length; i++)
+           {
+               array[i] = GetArrayFromConsole()[i];
+
+           } 
+
+        Console.WriteLine("Введите свое имя:");
+        User.F_Name = Console.ReadLine();
+        Console.WriteLine("Ваше имя {0}", User.F_Name);
+        Console.WriteLine("Введите свой возраст:");
+        User.Age = Convert.ToByte(Console.ReadLine());
+        Console.WriteLine("Ваше возраст {0}", User.Age);
 
         Console.WriteLine("Введите три своих любимых цвета");
-        var favcolors = new string[3]; 
-        for (int i = 0; i < 3; i++)
-        {
-           favcolors[i] =  ShowColor();
-            Console.WriteLine("Цыет {0}: {1}", i, favcolors[i]);
-        }
+          User.favcolor = new string[3]; 
+          for (int i = 0; i < 3; i++)
+          {
+            User.favcolor[i] = ShowColor(User.F_Name, User.Age);
+              Console.WriteLine("Цыет {0}: {1}", i + 1, User.favcolor[i]);
+          }
 
-    }
-    static int[] GetArray()
-    {
-        var result = new int[5];
-        int  temp = 0; 
-        for (int i = 0;i < result.Length; i++)
+      } 
+        static int[] GetArrayFromConsole()
         {
-            Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-            result[i] = int.Parse(Console.ReadLine());
-        }
-        for (int k = 0; k < result.Length - 1; k++)
-        {
-            for (int j = k + 1 ; j < result.Length; j++)
+            var result = new int[5];
+            
+            for (int i = 0; i < result.Length; i++)
             {
-                if (result[k] > result[j])
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+        return result;
+    }
+
+        static int[] SortArray(int[] array)
+    {
+        int temp = 0;
+        
+        for (int k = 0; k < array.Length - 1; k++)
+        {
+            for (int j = k + 1; j < array.Length; j++)
+            {
+                if (array[k] > array[j])
                 {
-                    temp = result[k];
-                    result[k] = result[j];
-                    result[j] = temp;
+                    temp = array[k];
+                    array[k] = array[j];
+                    array[j] = temp;
 
                 }
 
             }
         }
-        Console.WriteLine("Вывод отсортированного массива"); 
+        Console.WriteLine("Вывод отсортированного массива : ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i] + " ");
+        }
 
+        return array;
 
-        return result;
 
     }
 
 
 
 
-
-
 }
+
 
 
