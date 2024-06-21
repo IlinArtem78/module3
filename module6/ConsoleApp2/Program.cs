9b8471f4d25c52113ba32052ce00f784890f0e90
IlinArtem78 <artem.ilin1994@gmail.com>
2024-06-21 12:17:17 +0300

    Изучение классов и структур

---
+++ module6/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Human
+{
+    // Поля класса
+    public string name;
+    public int age;
+
+    // Метод класса
+    public void Greetings()
+    {
+        Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
+    }
+}
+
+struct Animal
+{
+    // Поля структуры
+    public string type;
+    public string name;
+    public int age;
+
+    // Метод структуры
+    public void Info()
+    {
+        Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
+    }
+}
